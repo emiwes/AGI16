0c4c54108f85ec3e79f776b7a21b97f9eae1696f
Arvid Sätterkvist <arvidsat@gmail.com>
2016-09-12 11:22:57 +0200

    Simple example server

---
+++ TowPow/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour {
+
+	public GameObject testObjectPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Q)) {
+			Spawn ();
+		}
+	}
+
+	void Spawn() {
+		Debug.Log ("Spawn");
+	}
+}
