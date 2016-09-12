719aa2886363a0853966978abc30ca0ee04164b8
Emil Westin <emiwes@kth.se>
2016-09-12 17:09:15 +0200

    First step toward HTC-vive hand interaction.

---
+++ TowPow/Assets/Scripts/htcvive/handController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handController : MonoBehaviour {
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input( (int)trackedObj.index); } }
+
+	// Use this for initialization
+	void Start () {
+		trackedObj.GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller == null) {
+			Debug.Log ("Controller not initalized.");
+			return;
+		}
+
+		if (controller.GetPressDown(gripButton)) {
+			Debug.Log ("Grip down!");
+		}
+		if (controller.GetPressUp(gripButton)) {
+			Debug.Log ("Grip up!");
+		}
+
+		if (controller.GetPressDown(triggerButton)) {
+			Debug.Log ("trigger down!");
+		}
+		if (controller.GetPressUp(triggerButton)) {
+			Debug.Log ("trigger up!");
+		}
+	}
+
+	private void OnTriggerEnter(Collider collider){
+		Debug.Log ("Trigger entered!");
+	}
+
+	private void OnTriggerExit(Collider collider){
+		Debug.Log ("Trigger exited!");
+	}
+}
