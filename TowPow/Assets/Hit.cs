19191c4068393260a43ae68acf412cacee4643bc
Calle <cstenson@kth.se>
2016-09-13 15:34:22 +0200

    movng creep, added model, some collision detection and started implementing shooting stuff

---
+++ TowPow/Assets/Hit.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hit : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+    //TODO
+        //detect end
+        //finished script?
+
+
+    public int startHealthPoints;
+    int currentHealthPoints;
+    float speed = 1f;
+
+    void Awake()
+    {
+        currentHealthPoints = startHealthPoints;
+    }
+    void Start()
+    {
+        //find zoombie
+        //set lives speed etc.
+        //healthPoints = 2;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            Debug.Log("space key was pressed");
+            Debug.Log("Kills gameobj:  " + gameObject.name);
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    //void CollisionDetection 
+
+    void OnCollisionEnter (Collision col)
+    {
+        //Debug.Log(col.gameObject.name);
+        if(col.gameObject.GetComponent<bulletType>() != null)
+        {
+            currentHealthPoints -= col.gameObject.GetComponent<bulletType>().Damage;
+            Debug.Log("collision hp:" + currentHealthPoints);
+            Destroy(col.gameObject);
+
+            if (currentHealthPoints <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+
+        
+    }
+}
---
+++ TowPow/Assets/bulletType.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletType : MonoBehaviour {
+
+    public int Damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
