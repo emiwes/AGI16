e2bc666a95b278a8b77ec126c9a8a191c953d303
Emil Westin <emiwes@kth.se>
2016-09-14 18:28:27 +0200

    Interaction pretty much done.

--- TowPow/Assets/Scripts/htcvive/handController.cs
+++ TowPow/Assets/Scripts/htcvive/handController.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class handController : MonoBehaviour {
 	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-
 	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 
 	private SteamVR_TrackedObject trackedObj;
 	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input ((int) trackedObj.index);} }
+
+
+	HashSet<interactableItem> objectsHoveringOver = new HashSet<interactableItem>();
+
+	private interactableItem closestItem;
+	private interactableItem interactingItem;
 
 	// Use this for initialization
 	void Start () {
@@ -23,9 +29,34 @@ public class handController : MonoBehaviour {
 
 		if (controller.GetPressDown(gripButton)) {
 			Debug.Log ("Grip down!");
+			float minDistance = float.MaxValue;
+			float distance;
+
+			foreach(interactableItem item in objectsHoveringOver){
+				distance = (item.transform.position - transform.position).sqrMagnitude;
+				if(distance < minDistance){
+					minDistance = distance;
+					closestItem = item;
+				}
+			}
+
+			interactingItem = closestItem;
+			closestItem = null;
+
+			// check if item is interacting when switching hands.
+			if(interactingItem){
+				if(interactingItem.IsInteracting()){
+					interactingItem.EndInteraction(this);
+				}
+
+				interactingItem.BeginInteraction(this);
+			}
 		}
-		if (controller.GetPressUp(gripButton)) {
+
+		if (controller.GetPressUp(gripButton) && interactingItem != null) {
 			Debug.Log ("Grip up!");
+			interactingItem.EndInteraction(this);
+
 		}
 
 		if (controller.GetPressDown(triggerButton)) {
@@ -38,9 +69,17 @@ public class handController : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider collider){
 		Debug.Log ("Trigger entered!");
+		interactableItem collidedItem = collider.GetComponent<interactableItem> ();
+		if (collidedItem) {
+			objectsHoveringOver.Add(collidedItem);
+		}
 	}
 
 	private void OnTriggerExit(Collider collider){
 		Debug.Log ("Trigger exited!");
+		interactableItem collidedItem = collider.GetComponent<interactableItem> ();
+		if (collidedItem) {
+			objectsHoveringOver.Remove(collidedItem);
+		}
 	}
 }
