ee6fe54c03186ad67cac85530cb4688232156988
HerrR <rbergel@kth.se>
2016-09-15 03:20:46 +0200

    Rough demo for automatic tower interaction with enemies.

---
+++ TowPow/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 100.0f;
+
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		transform.Translate(x, 0, z);
+	}
+}
---
+++ TowPow/Assets/Scripts/TowerTriggerActions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerTriggerActions : MonoBehaviour {
+	GameObject tower;
+	TowerCombat towerCombat;
+
+	void Start() {
+		tower = gameObject.transform.parent.gameObject;
+		towerCombat = tower.GetComponent<TowerCombat> ();
+	}
+
+	void OnTriggerEnter (Collider other){
+		if ((!towerCombat.nearbyEnemies.Contains (other.gameObject)) && (other.gameObject.tag == "Enemy"))
+			towerCombat.addNearbyEnemy (other.gameObject);
+	}
+
+	void OnTriggerStay(Collider other) {
+		towerCombat.updateClosestEnemy ();
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(towerCombat.nearbyEnemies.Contains(other.gameObject))
+			towerCombat.nearbyEnemies.Remove(other.gameObject);
+
+		towerCombat.checkLastLeave ();
+	}
+}
