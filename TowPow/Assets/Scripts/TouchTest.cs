fc754ce1b09e25de9556ee7ad3d7b133925535ea
Arvid Sätterkvist <arvidsat@gmail.com>
2016-09-20 16:59:11 +0200

    Spawn and despawn towers sorta works...

--- TowPow/Assets/Scripts/TouchTest.cs
+++ TowPow/Assets/Scripts/TouchTest.cs
@@ -34,7 +34,7 @@ namespace TouchScript
 
 				var i = 0;
 				foreach (GameObject tower in towerTypes) {
-					GameObject t = (GameObject) Instantiate(tower, new Vector3(i, 0, 0), Quaternion.identity);
+					GameObject t = (GameObject) Instantiate(tower, new Vector3(i, -3f, 0), Quaternion.identity);
 					NetworkServer.Spawn (t);
 					towers.Add (t);
 					i += 2;
@@ -48,6 +48,7 @@ namespace TouchScript
 			if (TouchManager.Instance != null)
 			{
 				TouchManager.Instance.TouchesBegan += touchesBeganHandler;
+				TouchManager.Instance.TouchesEnded += touchesEndedHandler;
 			}
 		}
 
@@ -56,23 +57,30 @@ namespace TouchScript
 			if (TouchManager.Instance != null)
 			{
 				TouchManager.Instance.TouchesBegan -= touchesBeganHandler;
+				TouchManager.Instance.TouchesEnded -= touchesEndedHandler;
 			}
 		}
 
 		private void touchesBeganHandler(object sender, TouchEventArgs e)
 		{
 			
-			foreach (var point in e.Touches)
-			{
+			foreach (var point in e.Touches) {
 //				Debug.Log ("pointHit: " + point.Hit);
 //				Debug.Log ("pointID: " + point.Id);
 //				Debug.Log ("pointSource: " + point.InputSource);
-				Debug.Log ("pointProps: " + point.Properties.Keys);
+//				Debug.Log ("pointProps: " + point.Properties.Keys);
 //				Debug.Log ("pointTags: " + point.Tags.HasTag("blue"));
 //				Debug.Log ("pointTarget: " + point.Target);
 //				Debug.Log ("sender: " + sender);
 //				Debug.Log ("e: " + e);
 				Spawn(point.Position, point.Tags);
+			}
+		}
+
+		private void touchesEndedHandler(object sender, TouchEventArgs e) {
+			Debug.Log ("End handler");
+			foreach (var point in e.Touches) {
+				Despawn (point.Position, point.Tags);
 			}
 		}
 
@@ -90,25 +98,32 @@ namespace TouchScript
 //		}
 
 		//[ClientRpc]
-		void Spawn(Vector2 position, Tags tags)
-		{
-			Debug.Log ("tags: " + tags.ToString());
+		void Spawn(Vector2 position, Tags tags) {
+			//Debug.Log ("tags: " + tags.ToString());
 			//GameObject testObject = (GameObject)Instantiate (Prefab, transform.position, transform.rotation);
 			//NetworkServer.Spawn (testObject);
 
-			GameObject obj = null;
-			Vector3 spawnPosition = topCamera.ScreenToWorldPoint(new Vector3(position.x, position.y, 8));
+			Vector3 spawnPosition = topCamera.ScreenToWorldPoint(new Vector3(position.x, position.y, 0));
+			spawnPosition.y = 0f;
 			Quaternion spawnRotation = transform.rotation;
 
 			foreach (GameObject tower in towers) {
 				if(tags.HasTag(tower.tag)){
 					if (Vector3.Distance (tower.transform.position, spawnPosition) > distanceThreshold) {
 						Debug.Log("ska spawnas");
-						tower.GetComponent<TowerSpawn>().SpawnAt (spawnPosition);
+						tower.GetComponent<TowerSpawn>().Spawn (spawnPosition);
 					}
 				}
 			}
+		}
 
+		void Despawn(Vector2 position, Tags tags) {
+			Debug.Log ("Despawn");
+			foreach (GameObject tower in towers) {
+				if(tags.HasTag(tower.tag)){
+					tower.GetComponent<TowerSpawn>().Despawn ();
+				}
+			}
 		}
 	}
 }
--- TowPow/Assets/Scripts/TowerSpawn.cs
+++ TowPow/Assets/Scripts/TowerSpawn.cs
@@ -5,19 +5,67 @@ public class TowerSpawn : MonoBehaviour {
 
 	public bool isActive;
 
+	public float spawnMovementSpeed;
+	public float spawnMovementDistance;
+
+	private float spawnStartTime;
+
+	private bool moving = false;
+	private Vector3 endPos;
+
+	private IEnumerator coroutine;
+
 	// Use this for initialization
 	void Start () {
+		// Start in despawned state
 		isActive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
-	public void SpawnAt(Vector3 position){
-		Debug.Log (position.ToString());
-		transform.position = position;
+	public void Despawn() {
+		if (moving)
+			return;
+		isActive = false;
+		// Sink into the ground;
+		StartCoroutine (MoveOverSeconds (new Vector3 (transform.position.x,  -spawnMovementDistance, transform.position.z), 3f));
+		moving = true;
+	}
+
+	public void Spawn(Vector3 position) {
+		if (moving)
+			return;
+		// TODO Set active after 5 seconds
 		isActive = true;
+		Vector3 startPos = new Vector3 (position.x, -spawnMovementDistance, position.z);
+		transform.position = startPos;
+		StartCoroutine (MoveOverSeconds (position, 3f));
+		moving = true;
+	}
+
+	public IEnumerator MoveOverSpeed (GameObject objectToMove, Vector3 end, float speed){
+		// speed should be 1 unit per second
+		while (objectToMove.transform.position != end)
+		{
+			objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, end, speed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+
+	public IEnumerator MoveOverSeconds (Vector3 end, float seconds)
+	{
+		float elapsedTime = 0;
+		Vector3 startingPos = transform.position;
+		while (elapsedTime < seconds)
+		{
+			transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		transform.position = end;
+		moving = false;
 	}
 }
