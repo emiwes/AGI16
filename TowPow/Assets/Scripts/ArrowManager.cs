ad8ee9669f00486cdf9cbda0668d4f499c3b3c80
OpenVR <OpenVR>
2016-09-20 17:02:28 +0200

    Added bow arrow scripts

---
+++ TowPow/Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowManager : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj ;
+
+    private GameObject currentArrow;
+
+    public GameObject Arrow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AttachArrow();
+	}
+
+    public void AttachArrow()
+    {
+        if(currentArrow == null)
+        {
+            currentArrow = Instantiate(Arrow);
+            currentArrow.transform.parent = trackedObj.transform;
+            currentArrow.transform.localPosition = new Vector3(0f, 0f, .342f);
+        }
+
+    }
+
+}
