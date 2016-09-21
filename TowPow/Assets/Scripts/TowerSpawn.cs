63b265a4e123eefa6a73f4c645f1cc4e35494835
Erik Forsberg <erik.c.forsberg@gmail.com>
2016-09-21 10:27:39 +0200

    Wait 2 secs before moving tower

--- TowPow/Assets/Scripts/TowerSpawn.cs
+++ TowPow/Assets/Scripts/TowerSpawn.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class TowerSpawn : MonoBehaviour {
 
 	public bool isActive;
+	public float spawnTime;
+	public float timeSinceSpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -12,12 +14,37 @@ public class TowerSpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isActive) {
+			timeSinceSpawn += Time.deltaTime;
+		}
 	}
 
 	public void SpawnAt(Vector3 position){
+		if (!isActive) {
+			transform.position = position;
+			isActive = true;
+			spawnTime = Time.time;
+		}
 		Debug.Log (position.ToString());
-		transform.position = position;
-		isActive = true;
+		Debug.Log ("SPawntime: " + spawnTime);
+
+		if (EligibleForRespawn ()) {
+			spawnTime = Time.time;
+			timeSinceSpawn = 0;
+			transform.position = position;
+		}
 	}
+
+	bool EligibleForRespawn(){
+		Debug.Log (timeSinceSpawn);
+		if (timeSinceSpawn < 2) {
+			Debug.Log ("time since spawn: " + timeSinceSpawn);
+			// det har inte gått två sekunder sedan vi senast placerade den
+			return false;
+		} else{
+			Debug.Log ("över 2sek sedan spawn");
+			return true;
+		};
+	}
+
 }
