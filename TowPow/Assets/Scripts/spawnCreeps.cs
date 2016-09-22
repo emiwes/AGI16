2e3a6f2c0dfffa726a44d4aba3ad73d213261096
Calle <cstenson@kth.se>
2016-09-22 10:17:49 +0200

    added fog, skybox and some movments

--- TowPow/Assets/Scripts/spawnCreeps.cs
+++ TowPow/Assets/Scripts/spawnCreeps.cs
@@ -5,13 +5,14 @@ public class spawnCreeps : MonoBehaviour {
 
     public GameObject enemyPrefab;
     public GameObject pathPrefab;
-    public GameObject startPosition;
+    //public GameObject startPosition;
+    //public GameObject spawObj;
 
     // Use this for initialization
     void Start () {
-        
-       
-	}
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,12 +26,20 @@ public class spawnCreeps : MonoBehaviour {
 
     void spawnZombie()
     {
-        //Instatiate Creep
-        GameObject temp_enemy = Instantiate(enemyPrefab, startPosition.transform.position, Quaternion.identity) as GameObject;
-        //set path as a child to creepSpawner
-        temp_enemy.transform.parent = GameObject.Find("enemySpawner").transform;
+        
+        //Debug.Log("spwn!!");
+
         //Instatiate path that creep should go
         GameObject temp_path = Instantiate(pathPrefab) as GameObject;
+
+        //Get startPosition
+        //Transform startPosition = temp_path.transform.GetChild(0);
+
+        //Instatiate Creep
+        GameObject temp_enemy = Instantiate(enemyPrefab, temp_path.transform.GetChild(0).position, Quaternion.identity) as GameObject;
+        //set path as a child to spawner Gameobject.
+        temp_enemy.transform.parent = this.transform;
+        
         //set path as a child to Creep
         temp_path.transform.parent = temp_enemy.transform;
 
