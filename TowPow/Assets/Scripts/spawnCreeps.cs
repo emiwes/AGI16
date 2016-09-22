8e40bffe23cd6cc44e7ffc754c2f701eb1622cf7
Calle <cstenson@kth.se>
2016-09-22 18:31:55 +0200

    changed movmentscript

--- TowPow/Assets/Scripts/spawnCreeps.cs
+++ TowPow/Assets/Scripts/spawnCreeps.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class spawnCreeps : MonoBehaviour {
 
     public GameObject enemyPrefab;
-    public GameObject pathPrefab;
+    [HideInInspector]
+    public Transform target;
+    //public GameObject pathPrefab;
     //public GameObject startPosition;
     //public GameObject spawObj;
 
     // Use this for initialization
     void Start () {
+        target = this.transform.Find("target");
 		}
 	
 	// Update is called once per frame
@@ -28,21 +31,23 @@ public class spawnCreeps : MonoBehaviour {
         //Debug.Log("spwn!!");
 
         //Instatiate path that creep should go
-        GameObject temp_path = Instantiate(pathPrefab) as GameObject;
+        //GameObject temp_path = Instantiate(pathPrefab) as GameObject;
 
         //Get startPosition
         //Transform startPosition = temp_path.transform.GetChild(0);
 
         //Instatiate Creep
-        GameObject temp_enemy = Instantiate(enemyPrefab, temp_path.transform.GetChild(0).position, Quaternion.identity) as GameObject;
+        GameObject temp_enemy = Instantiate(enemyPrefab, this.transform.position, Quaternion.identity) as GameObject;
         //set path as a child to spawner Gameobject.
+        temp_enemy.GetComponent<enemyMovement>().target = target;
         temp_enemy.transform.parent = this.transform;
+
         
         //set path as a child to Creep
-        temp_path.transform.parent = temp_enemy.transform;
+        //temp_path.transform.parent = temp_enemy.transform;
 
         //attack path to creep
-        temp_enemy.GetComponent<SplineController>().SplineRoot = temp_path;
+        //temp_enemy.GetComponent<SplineController>().SplineRoot = temp_path;
         
 
     }
