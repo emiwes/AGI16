b7a3f24b25e3de93185ce99ade117e69c5c49d9b
HerrR <rbergel@kth.se>
2016-09-25 23:17:19 +0200

    Pirate model imported. - Press spacebar for emote - Press v for victory jump

---
+++ TowPow/Assets/Scripts/PirateAnimationController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PirateAnimationController : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			anim.Play ("Forgot I was a pirate");
+		
+		if (Input.GetKeyDown ("v"))
+			anim.Play ("Victory Jump");
+	}
+}
