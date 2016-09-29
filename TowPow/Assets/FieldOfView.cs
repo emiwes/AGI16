e678a6284202882c3ebe65cc9437c441e7bca130
Calle <cstenson@kth.se>
2016-09-29 10:03:55 +0200

    Tried to make it rotate by arrowkeys but something is wrong...

--- TowPow/Assets/FieldOfView.cs
+++ TowPow/Assets/FieldOfView.cs
@@ -8,45 +8,76 @@ public class FieldOfView : MonoBehaviour {
     public int length;
     public GameObject canvas;
     public GameObject segmentPrefab;
+    private bool isInstatiated = false;
+    private GameObject circleSegment;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("space"))
+
+        if (Input.GetKeyDown("left"))
         {
-            instatiateCirclesegment(true);
-            instatiateCirclesegment(false);
+            Vector3 newRot = transform.rotation.eulerAngles;
+            newRot.y += 10;
+            this.transform.eulerAngles = newRot;
+        }else if (Input.GetKeyDown("right"))
+        {
+            Vector3 newRot = transform.rotation.eulerAngles;
+            newRot.y -= 10;
+            this.transform.eulerAngles = newRot;
+        }
+        if (!isInstatiated)
+        {
+            circleSegment =  instatiateCirclesegment(true);
+            isInstatiated = true;
+        }
+        else
+        {
+            Vector3 segmentEuler = circleSegment.transform.rotation.eulerAngles;
+            //Vector3 boxEuler = this.transform.rotation.eulerAngles;
+            //Debug.Log(boxEuler.y + (viewAngleDegree / 2) - 100);
+            if (segmentEuler.z != (this.transform.rotation.y + (viewAngleDegree / 2) - 100))
+            {
+                segmentEuler.x = 0;
+                segmentEuler.z = (this.transform.rotation.y + (viewAngleDegree / 2) - 100);
+
+
+                //update
+                circleSegment.transform.rotation = Quaternion.Euler(segmentEuler);
+                Debug.Log("updates field of view :" + segmentEuler.ToString());//update circle segment
+            }
         }
 	
 	}
 
-    void instatiateCirclesegment(bool isClockwise)
+    GameObject instatiateCirclesegment(bool isClockwise)
     {
         Vector3 position = new Vector3(0, 0, 0);
         //translates worldposition to position on the canvas
        
-        Debug.Log(rt);
         Vector3 p1 = RectTransformUtility.WorldToScreenPoint(Camera.main, position);
 
         Vector3 rotation = new Vector3(0,0,0);
-        rotation.z = this.transform.rotation.y - 100; // why -100?!
+        rotation.z = this.transform.rotation.y + (viewAngleDegree/2) - 100; // why -100?!
+        Debug.Log("rotation z: " + rotation.ToString());
 
         
         GameObject segment = Instantiate(segmentPrefab, p1, Quaternion.Euler(rotation)) as GameObject;
 
         //retrivee component Image script
         Image imageScript = segment.GetComponent<Image>();
-        imageScript.fillAmount = (viewAngleDegree / 2) / 360;
+        imageScript.fillAmount = viewAngleDegree / 360;
         imageScript.fillClockwise = isClockwise;
-        Debug.Log("sets view angle to :" + (viewAngleDegree / 2) / 360);
 
         //set the object parent to the canvas
         segment.transform.SetParent(canvas.transform);
+
+        return gameObject;
     }
 
 
