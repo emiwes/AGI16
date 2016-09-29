621b03bc0d3b6e59b0aa744d40cf695c080e9ba4
OpenVR <OpenVR>
2016-09-29 10:46:41 +0200

    Start on grenade interaction

---
+++ TowPow/Assets/GrenadeSprintScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VRTK
+{
+    public class GrenadeSprintScript : VRTK_InteractableObject
+    {
+        public bool grenadeActive = false;
+
+        // Update is called once per frame
+        void Update () {
+            if (base.IsGrabbed())
+            {
+                grenadeActive = true;
+                Destroy(GetComponent<HingeJoint>());
+            }
+	        
+	    }
+    }
+}
---
+++ TowPow/Assets/Scripts/Grenade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VRTK
+{
+    public class Grenade : VRTK_InteractableObject
+    {
+	    // Update is called once per frame
+	    void Update () {
+            if (GetComponentInChildren<GrenadeSprintScript>().grenadeActive)
+            {
+                Debug.Log("BOOM");
+            }
+	
+	    }
+    }
+}
