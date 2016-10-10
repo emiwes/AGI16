13469f2a22b61189c82da63ac56e858cf3f30381
Emil Westin <emiwes@kth.se>
2016-10-10 16:09:02 +0200

    testing for PixelSense

--- TowPow/Assets/Scripts/Enemy/EnemyCombat.cs
+++ TowPow/Assets/Scripts/Enemy/EnemyCombat.cs
@@ -5,18 +5,33 @@ using UnityEngine.Networking;
 
 public class EnemyCombat : NetworkBehaviour {
 	public float health = 100;
+	public GameObject coinPrefab;
+	public bool isNotDead = true;
 
 	public void takeDamage (float damage) {
 		health -= damage;
-		if (health <= 0)
+		if (health <= 0 && isNotDead){
+			isNotDead = false;
 			CmdDie ();
+		}
 	}
+
 	[Command]
 	void CmdDie(){
+		
 		Animator animator = gameObject.GetComponent<Animator> ();
 //		animator.SetBool ("Die", true);
+
 		animator.Play ("Die");
+		spawnCoin();
 		Destroy (gameObject, animator.GetCurrentAnimatorStateInfo (0).length);
+	}
+
+	void spawnCoin (){
+
+		GameObject coin = Instantiate ( coinPrefab, this.transform.position, coinPrefab.transform.rotation ) as GameObject;
+		NetworkServer.Spawn (coin);
+
 	}
 
 }
