8a2b76755c6f6265b5438ff58d9f9e00f203fed2
admin <admin>
2016-10-12 17:24:53 +0200

    Graphic raycaster now active on HUD. Trying to increment money on coin click.

--- TowPow/Assets/Scripts/Enemy/CoinClick.cs
+++ TowPow/Assets/Scripts/Enemy/CoinClick.cs
@@ -7,5 +7,6 @@ public class CoinClick : MonoBehaviour {
 	public void DestroyCoin(){
 		Debug.Log ("Coin was clicked");
 		Destroy (this.transform.gameObject);
+		GameObject.Find ("GameHandler").GetComponent<GameScript> ().moneyCounter += 10;
 	}
 }
