b817de665917df9ab2d3a9f77bc547216a27d112
Emil Westin <emiwes@kth.se>
2016-10-18 12:18:30 +0200

    Error handling on coin spawning and enemies

--- TowPow/Assets/EnemyHandler.cs
+++ TowPow/Assets/EnemyHandler.cs
@@ -4,6 +4,9 @@ using UnityEngine.Networking;
 
 public class EnemyHandler : NetworkBehaviour {
 
+	public Camera topCamera;
+	public GameObject coinPrefab;
+
 	[Command]
 	public void CmdDie(GameObject pirate){
 		Debug.Log ("DYING...");
@@ -15,10 +18,16 @@ public class EnemyHandler : NetworkBehaviour {
 		//Play death sound
 		float vol = Random.Range (.5f, 1f);
 		enemyCombatComponent.source.PlayOneShot(enemyCombatComponent.deathSoundArray[Random.Range(0, enemyCombatComponent.deathSoundArray.Length)],vol);
-		//Also change kill counter on all clients
-		Destroy (pirate, animator.GetCurrentAnimatorStateInfo (0).length);
-		GameObject.Find ("GameHandler").GetComponent<GameScript> ().killCounter += 1;
 
-		gameObject.GetComponent<CoinHandler>().CmdSpawnCoin (transform.position);
+
+		//gameObject.GetComponent<CoinHandler>().CmdSpawnCoin (transform.position);
+
+		GameObject coin = (GameObject)Instantiate(coinPrefab, topCamera.WorldToScreenPoint(pirate.transform.position), Quaternion.identity);
+		coin.transform.SetParent(GameObject.Find("HUDCanvas").transform);
+		NetworkServer.Spawn (coin);
+
+		Destroy (pirate, animator.GetCurrentAnimatorStateInfo (0).length);
+		//Also change kill counter on all clients
+		GameObject.Find ("GameHandler").GetComponent<GameScript> ().killCounter += 1;
 	}
 }
