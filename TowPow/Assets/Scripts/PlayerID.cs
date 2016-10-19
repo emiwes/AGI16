c2b6fac25f8ee91c4c05f03ae69e9c79cfa8017e
Emil Westin <emiwes@kth.se>
2016-10-19 14:39:32 +0200

    Removed duplicate net identity on FOV, cleaned up script where local player net ID is set.

--- TowPow/Assets/Scripts/PlayerID.cs
+++ TowPow/Assets/Scripts/PlayerID.cs
@@ -5,52 +5,10 @@ using UnityEngine.Networking;
 public class PlayerID : NetworkBehaviour {
 
 	public NetworkInstanceId myNetId;
-	[SyncVar] public string playerUniqueIdentity;
 
 	public override void OnStartLocalPlayer(){
 		myNetId = GetComponent<NetworkIdentity> ().netId;
 		GameObject.Find ("LocalPlayerNetId").GetComponent<LocalPlayerNetId> ().netId = myNetId;
 		Debug.Log ("OnStart Player net ID is: " + GameObject.Find ("LocalPlayerNetId").GetComponent<LocalPlayerNetId> ().netId.ToString());
-
-//		GetNetIdentity ();
-//
-//		SetIdentity ();
-	}
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-//	void Update () {
-//		if(transform.name == "" || transform.name == "PlayerObject(Clone)"){
-//			SetIdentity ();
-//		}
-//	}
-
-	void SetIdentity(){
-		if (!isLocalPlayer) {
-			transform.name = playerUniqueIdentity;
-		} 
-		else {
-			transform.name = MakeUniqueIdentity ();
-		}
-	}
-
-
-	void GetNetIdentity(){
-		myNetId = GetComponent<NetworkIdentity> ().netId;
-		CmdTellServerMyNetId (MakeUniqueIdentity());
-	}
-
-	string MakeUniqueIdentity(){
-		string uniqueName = "Player " + myNetId.ToString ();
-		return uniqueName;
-	}
-
-	[Command]
-	void CmdTellServerMyNetId(string name){
-		playerUniqueIdentity = name;
 	}
 }
