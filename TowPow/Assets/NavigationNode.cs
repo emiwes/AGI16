e449265f0ad890e77a49e8b458a48b367a36ab95
Calle <cstenson@kth.se>
2016-10-20 10:41:22 +0200

    Navigation Model through Nodes:)

---
+++ TowPow/Assets/NavigationNode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NavigationNode : MonoBehaviour {
+
+    public List<Transform> childs = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //change to one of the childs of the node
+
+        //changes target to a random of the childs
+        int RandomChild = Random.Range(0, childs.Count);
+        col.gameObject.GetComponent<EnemyMovement>().target = childs[RandomChild];
+    }
+}
--- TowPow/Assets/Scripts/Enemy/spawnEnemy.cs
+++ TowPow/Assets/Scripts/Enemy/spawnEnemy.cs
@@ -9,7 +9,7 @@ public class spawnEnemy : NetworkBehaviour
 
     public GameObject enemyPrefab;
     //[HideInInspector]
-    public List<Transform> targetPoints = new List<Transform>();
+    //public List<Transform> targetPoints = new List<Transform>();
     public List<Transform> startPoints = new List<Transform>();
     //public GameObject pathPrefab;
     //public GameObject startPosition;
@@ -49,13 +49,13 @@ public class spawnEnemy : NetworkBehaviour
 
     }
 
-    public void spawnSingleEnemy(int spawnPoint, int targetPoint)
+    public void spawnSingleEnemy(int spawnPoint)
     {
 
         //Instatiate Enemy
         GameObject temp_enemy = Instantiate(enemyPrefab, startPoints[spawnPoint].position, Quaternion.identity) as GameObject;
         //set path as a child to spawner Gameobject.
-        temp_enemy.GetComponent<EnemyMovement>().target = targetPoints[targetPoint];
+        //temp_enemy.GetComponent<EnemyMovement>().target = targetPoints[targetPoint];
         temp_enemy.transform.SetParent(this.transform);
         //to spawn on all clients.
 		//----without slider
@@ -72,9 +72,9 @@ public class spawnEnemy : NetworkBehaviour
     public void spawnSingleEnemy()
     {
         int spawnPoint = Random.Range(0, startPoints.Count);//max exclusive for int, inclusive for floats
-        int targetPoint = Random.Range(0, targetPoints.Count);//max exclusive for int, inclusive for floats
+       // int targetPoint = Random.Range(0, targetPoints.Count);//max exclusive for int, inclusive for floats
 
         //Debug.Log("Spawn point randomized: "+ spawnPoint);
-        spawnSingleEnemy(spawnPoint, targetPoint);
+        spawnSingleEnemy(spawnPoint);
     }
 }
