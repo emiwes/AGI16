1b6b6b29e8ff90ec2c95d86ca0bb7b9148ad5694
admin <admin>
2016-10-28 14:19:29 +0200

    bug fix for enemyhelath

--- TowPow/Assets/Scripts/Enemy/OnEnemyReachGoal.cs
+++ TowPow/Assets/Scripts/Enemy/OnEnemyReachGoal.cs
@@ -7,13 +7,10 @@ public class OnEnemyReachGoal : MonoBehaviour {
 
     private GameScript GameScriptRef;
 
-	void Awake() {
-        GameScriptRef = GameObject.Find("GameHandler").GetComponent<GameScript>();
-
-    }
-
 	void OnTriggerEnter(Collider other) {
-
+		if (!GameScriptRef) {
+			GameScriptRef = GameObject.Find("GameHandler").GetComponent<GameScript>();
+		}
 		if (other.gameObject.tag == "Enemy") {
 			GameScriptRef.EnemyReachedGoal(other.gameObject);
 		}
