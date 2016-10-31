be9000b2a26100c2edebc42bd7d17396751b406a
Arvid Sätterkvist <arvidsat@kth.se>
2016-10-31 22:21:42 +0100

    Fire and Ice exposions... maybe?

--- TowPow/Assets/Scripts/ProjectileDamage.cs
+++ TowPow/Assets/Scripts/ProjectileDamage.cs
@@ -13,54 +13,57 @@ public class ProjectileDamage : MonoBehaviour {
 	public float areaOfEffectRadius = 0;
 	public float areaOfEffectExpandTime = 1;
 	public GameObject projectileAoePrefab;
+    public GameObject projectileHitEffectPrefab;
 
 	void Awake(){
 		source = gameObject.GetComponent<AudioSource> ();
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "Enemy") {
-			// Do the projectile damage/effects on the target
-			// other.gameObject.GetComponent<EnemyCombat> ().HandleIncomingProjectile(this);
 
-			// Create area of effect if we have a defined radius
-			if(areaOfEffectRadius > 0){
-				// Instantiate the AoE prefab (it grows by itself)
-				GameObject projectileAoe = (GameObject)Instantiate(projectileAoePrefab, other.transform.position, Quaternion.identity);
-				SphereScript ss = projectileAoe.GetComponent<SphereScript> ();
-				ss.sphereRadius = areaOfEffectRadius;
-				ss.spawnedByProjectile = gameObject;
-				ss.damage = damage;
-				ss.damageOverTimeSeconds = damageOverTimeSeconds;
-				ss.speedMultiplier = speedMultiplier;
-				ss.speedOverTimeSeconds = speedOverTimeSeconds;
-				ss.morphEnemyToMaterial = morphEnemyToMaterial;
-				ss.areaOfEffectRadius = areaOfEffectRadius;
-				ss.areaOfEffectExpandTime = areaOfEffectExpandTime;
-				ss.projectileAoePrefab = projectileAoePrefab;
+        // Create are of effect if we have a defined radius
+        if (areaOfEffectRadius > 0) {
+            AOE();
+        } else if (other.gameObject.tag == "Enemy") {
+            other.gameObject.GetComponent<EnemyCombat>().HandleIncomingProjectile(this);
+        }
 
-			} else{
-				// Otherwise, do the projectile damage/effects on the target
-				other.gameObject.GetComponent<EnemyCombat> ().HandleIncomingProjectile(this);
-			}
-            
-            //if it has trailing smoke remove it after the removal of the projectile
-            if (gameObject.transform.FindChild("movingSmoke"))
-            {
-                Transform smoke = gameObject.transform.FindChild("movingSmoke");
-                smoke.parent = null;
-                smoke.GetComponent<ParticleSystem>().Stop(true);
+        //if it has trailing smoke remove it after the removal of the projectile
+        if (gameObject.transform.FindChild("movingSmoke")) {
+            Transform smoke = gameObject.transform.FindChild("movingSmoke");
+            smoke.parent = null;
+            smoke.GetComponent<ParticleSystem>().Stop(true);
 
-                Destroy(smoke.gameObject, 2.0f);
-            }
-          
-			//Play sound if arrow (== tag friendly)
-			if (gameObject.tag == "Friendly") {
-				source.PlayOneShot (hitSound, .7f);
-			}
+            Destroy(smoke.gameObject, 2.0f);
+        }
 
-			// Destroy the projectile
-			Destroy (gameObject);
-		}
+        //Play sound if arrow (== tag friendly)
+        if (gameObject.tag == "Friendly") {
+            source.PlayOneShot(hitSound, .7f);
+        }
+
+        // Destroy the projectile
+        Destroy (gameObject);
 	}
+
+    void AOE() {
+        // Instantiate the AoE prefab (it grows by itself)
+        GameObject projectileAoe = (GameObject)Instantiate(projectileAoePrefab, transform.position, Quaternion.identity);
+        SphereScript ss = projectileAoe.GetComponent<SphereScript>();
+        ss.sphereRadius = areaOfEffectRadius;
+        ss.spawnedByProjectile = gameObject;
+        ss.damage = damage;
+        ss.damageOverTimeSeconds = damageOverTimeSeconds;
+        ss.speedMultiplier = speedMultiplier;
+        ss.speedOverTimeSeconds = speedOverTimeSeconds;
+        ss.morphEnemyToMaterial = morphEnemyToMaterial;
+        ss.areaOfEffectRadius = areaOfEffectRadius;
+        ss.areaOfEffectExpandTime = areaOfEffectExpandTime;
+        ss.projectileAoePrefab = projectileAoePrefab;
+
+        // If there's an explosion effect...
+        if (projectileHitEffectPrefab) {
+            GameObject explosion = (GameObject)Instantiate(projectileHitEffectPrefab, transform.position, Quaternion.identity);
+        }
+    }
 }
