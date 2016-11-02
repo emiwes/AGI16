20036779d7871c7323c62a82b78fa024912ff409
Arvid Sätterkvist <arvidsat@kth.se>
2016-11-02 16:08:48 +0100

    poster scene

---
+++ TowPow/Assets/Scripts/Screenshot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+    void Update() {
+
+        if(Input.GetKeyUp(KeyCode.Space)) {
+            Debug.Log("Screenshot");
+            Application.CaptureScreenshot("screenshot.png", 4);
+        }
+    }
+}
