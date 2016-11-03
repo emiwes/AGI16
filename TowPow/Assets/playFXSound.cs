68ce862439b21b29aaa11e219fb58ae4cd8f24e8
Anton Sivertsson <sivertsson.anton@gmail.com>
2016-11-03 12:59:56 +0100

    Update sound fx

---
+++ TowPow/Assets/playFXSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playFXSound : MonoBehaviour {
+
+	public AudioClip FXSound;
+	private AudioSource source;
+	private float volLowRange = .2f;
+	private float volHighRange = .5f;
+
+
+	void Awake() {
+		source = GetComponent<AudioSource>();
+	}
+	// Use this for initialization
+	void Start () {
+		float vol = Random.Range (volLowRange, volHighRange);
+		source.PlayOneShot(FXSound,vol);
+	}
+
+}
